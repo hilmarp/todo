3472210f4ff80ecb39eaaaba2e205e9c7ba3f909
Hilmar <hilmar@hilmarp.com>
2015-01-07 14:10:11 +0000

    App ready

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ Express backend og Angular frontend
 
 1. git clone
 2. npm install
+3. bower install
--- public/core.js
+++ public/core.js
@@ -1,6 +1,6 @@
 var todoApp = angular.module('todoApp', []);
 
-function mainController($scope, $http) {
+todoApp.controller('mainController', ['$scope', '$http', function($scope, $http) {
 	$scope.formData = {};
 
 	// Birta öll todo á síðunni
@@ -28,7 +28,7 @@ function mainController($scope, $http) {
 	};
 
 	// Eyða todo
-	$scope.deleteTodo = function() {
+	$scope.deleteTodo = function(id) {
 		$http.delete('/api/todos/' + id)
 			.success(function(data) {
 				$scope.todos = data;
@@ -38,4 +38,44 @@ function mainController($scope, $http) {
 				console.log('Error: ' + data);
 			});
 	};
-}
+}]);
+
+// function mainController($scope, $http) {
+// 	$scope.formData = {};
+
+// 	// Birta öll todo á síðunni
+// 	$http.get('/api/todos')
+// 		.success(function(data) {
+// 			$scope.todos = data;
+// 			console.log('Success: ' + data);
+// 		})
+// 		.error(function(data) {
+// 			console.log('Error: ' + data);
+// 		});
+
+// 	// Þegar formi er POSTað þá senda gögnin til API
+// 	$scope.createTodo = function() {
+// 		$http.post('/api/todos', $scope.formData)
+// 			.success(function(data) {
+// 				// Hreinsa input formið
+// 				$scope.formData = {};
+// 				$scope.todos = data;
+// 				console.log('Success: ' + data);
+// 			})
+// 			.error(function(data) {
+// 				console.log('Error: ' + data);
+// 			});
+// 	};
+
+// 	// Eyða todo
+// 	$scope.deleteTodo = function() {
+// 		$http.delete('/api/todos/' + id)
+// 			.success(function(data) {
+// 				$scope.todos = data;
+// 				console.log('Success: ' + data);
+// 			})
+// 			.error(function(data) {
+// 				console.log('Error: ' + data);
+// 			});
+// 	};
+// };
